33697e12a5fe5be4e38ab9cb78a1fd429e9a5d37
Piotr Kocia <p.kocia@wp.pl>
2019-12-01 22:02:04 +0100

    Update D1_1.cpp

--- D1_1.cpp
+++ D1_1.cpp
@@ -2,8 +2,8 @@
 
 using i64 = long long;
 
-i64 min(i64 const a, i64 const b) {
-    return a < b ? a : b;
+i64 max(i64 const a, i64 const b) {
+    return a > b ? a : b;
 }
 
 int main() {
@@ -13,8 +13,8 @@ int main() {
     i64 module_mass;
     while(std::cin >> module_mass) {
         // The input numbers are large enough that the result is never < 0
-        //  thus that `min` isn't necessary
-        counter_upper += min(module_mass / 3 - 2, 0);
+        //  thus that `max` isn't necessary
+        counter_upper += max(module_mass / 3 - 2, 0);
     }
 
     std::cout << counter_upper;
