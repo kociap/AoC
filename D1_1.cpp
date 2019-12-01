54f31b53aba45054db0b4b743450ce56c2e34bea
Piotr Kocia <p.kocia@wp.pl>
2019-12-01 21:58:16 +0100

    Create D1_1.cpp

---
+++ D1_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using i64 = long long;
+
+i64 min(i64 const a, i64 const b) {
+    return a < b ? a : b;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+    i64 counter_upper = 0;
+    i64 module_mass;
+    while(std::cin >> module_mass) {
+        // The input numbers are large enough that the result is never < 0
+        //  thus that `min` isn't necessary
+        counter_upper += min(module_mass / 3 - 2, 0);
+    }
+
+    std::cout << counter_upper;
+    return 0;
+}
